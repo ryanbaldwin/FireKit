95d3e4b18ec6ecece52f53a60023a36d7a01b0d0
Ryan Baldwin <rbaldwin@ehealthinnovation.org>
2017-09-25 09:36:11 -0400

    swift4: FHIRAbstractResource now ensures it's decoding the proper type.

--- firekit/fhir-parser-resources/fhir-1.6.0/swift-4.0/FHIRAbstractResource.swift
+++ firekit/fhir-parser-resources/fhir-1.6.0/swift-4.0/FHIRAbstractResource.swift
@@ -9,7 +9,7 @@
 // Copyright @ 2017 Bunnyhug. All rights fall under Apache 2
 //
 import Foundation
-
+import Realm
 
 /**
  *  Abstract superclass for all FHIR resource models.
@@ -23,6 +23,40 @@ open class FHIRAbstractResource: FHIRAbstractBase {
     @objc open dynamic var _versionId: String?
     override open var description: String {
         return "<\(type(of: self).resourceType)>"
+    }
+    
+    required public init() {
+        super.init()
+    }
+    
+    required public init(value: Any, schema: RLMSchema) {
+        super.init(value: value, schema: schema)
+    }
+    
+    required public init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init(realm: realm, schema: schema)
+    }
+    
+    // Codable - Default implementations which do nothing.
+    public required init(from decoder: Decoder) throws {
+        super.init()
+        
+        let thisResourceType = type(of: self).resourceType
+        guard thisResourceType != Resource.resourceType else {
+            // Any resource can be decoded. Example, a MedicationOrder into a BundleEntry.
+            return
+        }
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        guard let decodedResourceType = try container.decodeIfPresent(String.self, forKey: .resourceType) else { return }
+        
+        guard decodedResourceType == thisResourceType else {
+            let context = DecodingError.Context(codingPath: decoder.codingPath,
+                                                debugDescription: """
+                Attempting to decode a \(decodedResourceType) into a \(thisResourceType)
+                """)
+            throw DecodingError.typeMismatch(type(of: self), context)
+        }
     }
     
     public override func encode(to encoder: Encoder) throws {
