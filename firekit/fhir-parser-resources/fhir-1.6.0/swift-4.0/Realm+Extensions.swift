263ede4b74a82052b551c35ac47a4362401b3ef8
ryanbaldwin <ryanbaldwin@gmail.com>
2017-11-09 18:00:02 -0500

    regressed a bug a few weeks ago
    accidentally busted upsert(_ resources:). Tests pass, but it crashed on the device. This re-instates that original fix in 701bcf8

--- firekit/fhir-parser-resources/fhir-1.6.0/swift-4.0/Realm+Extensions.swift
+++ firekit/fhir-parser-resources/fhir-1.6.0/swift-4.0/Realm+Extensions.swift
@@ -15,8 +15,7 @@ extension Realm {
     }
     
     public func resources<T: Resource>(_ ofType: T.Type, withIds ids: [String]) -> Results<T> {
-        let idsToFetch = Set(ids).map { "'\($0)'" }.joined(separator: ",")
-        return objects(ofType).filter("id IN { \(idsToFetch) }")
+        return objects(ofType).filter(NSPredicate(format: "id IN %@", Set(ids)))
     }
     
     /// Performs an intelligent update-or-insert action based on the `id` field of the provided resource.
@@ -27,7 +26,7 @@ extension Realm {
     @discardableResult public func upsert<T>(_ resource: T) -> T where T: Resource {
         guard let resourceId = resource.id else {
             let message = "Cannot upsert this Resource because it did not contain an ID. " +
-                          "Will insert Resource (\(T.self)) with key\(resource.pk))"
+            "Will insert Resource (\(T.self)) with key\(resource.pk))"
             print(message)
             add(resource)
             return resource
@@ -47,19 +46,23 @@ extension Realm {
     /// - Parameter resources: The resourses to be updated or inserted.
     /// - Warning: This method may only be called during a write transaction
     /// - Returns: The list of new and now-up-to-date-existing resources, in the same order.
-    @discardableResult public func upsert<T: Resource>(_ resources: [T]) -> [T] {
+    @discardableResult public func upsert<T>(_ resources: [T]) -> [T] where T: Resource {
         guard resources.count > 0 else {
             return resources
         }
         
         // First, we want to build up a map of `id` to Resource, for quick retrieval & replacement.
         var resourcesMap: [String: T] = [:]
-        resources.lazy.filter { $0.id != nil }.forEach { resourcesMap[$0.id!] = $0 }
-        let resourcesToAdd = resources.lazy.filter { $0.id == nil }
+        resources.lazy.filter { resource in resource.id != nil }
+            .forEach { resource in resourcesMap[resource.id!] = resource }
+        let resourcesToAdd = resources.lazy.filter { resource in resource.id == nil }
         
         // Next, get all the local T where each respective's `id` matches on from the map
         let resourceIds = Set(resourcesMap.keys)
-        let existingResources = self.resources(type(of: resources.first!), withIds: Array(resourceIds))
+        
+        // lame string concatenation here because there's no good shorthand
+        // to get a set in the proper realm query format.
+        let existingResources = objects(T.self).filter(NSPredicate(format: "id IN %@", resourceIds))
         
         // For any T that exists locally, we simply want to re-populate it from what's passed in.
         // This is where that nice map above comes in handy.
